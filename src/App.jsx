8f6891b2f4cb337fb887c2b77b8affae00d194cd
juanruben <juanruben@gmail.com>
2020-02-13 11:04:49 -0300

    Configura gestión básica de idiomas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { setTranslations, setDefaultLanguage, translate } from 'react-switch-lang';
 import './App.scss';
 
-function App() {
-    return (
-        <div className="app">
-            Texto
-        </div>
-    );
+import es from './languages/es.json';
+import en from './languages/en.json';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        setTranslations({ es, en });
+        setDefaultLanguage('en');
+    }
+
+    render() {
+        const { t } = this.props;
+        return (
+            <div className="app">
+                {t('app.text')}
+            </div>
+        );
+    }
 }
 
-export default App;
+App.propTypes = {
+    t: PropTypes.func.isRequired,
+};
+
+export default translate(App);
